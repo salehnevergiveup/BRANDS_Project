e9274682c33730ee1c1f3ee02280b7fb1471a7b1
Saleh Ahmed <82533819+salehnevergiveup@users.noreply.github.com>
2023-09-12 10:36:00 +0800

    Add files via upload

---
+++ project one/Javascript/main.js
@@ -0,0 +1,13 @@
+
+var r = document.querySelector(':root');
+function normal() {
+  r.style.setProperty('--yellow', '#ffe500');
+  r.style.setProperty('--backgroundc-one', '#324651');
+  r.style.setProperty('--backgroundc-two', '#fff');
+
+}
+function darkmode() {
+  r.style.setProperty('--yellow', '#c8d6e5');
+  r.style.setProperty('--backgroundc-one', '#2d3436');
+  r.style.setProperty('--backgroundc-two', '#747d8c');
+}
